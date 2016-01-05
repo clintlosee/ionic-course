e1240eafc1f806a1959289b9562bb002f3ffd56f
Clint Losee <clint.losee@gmail.com>
2016-01-05 09:23:06 -0700

    Added tedrssapp to project.

---
+++ tedrssapp/www/js/controllers.js
@@ -0,0 +1,33 @@
+var app = angular.module('tedrssapp.controllers', []);
+
+app.controller('FeedCtrl', function ($scope, $ionicLoading, FeedService) {
+	console.log("Loading FeedCtrl");
+
+    $ionicLoading.show({template: 'Loading feed...'});
+	$scope.feed = FeedService;
+	$scope.feed.loadFeed().then(function() {
+        $ionicLoading.hide();
+    });
+
+	$scope.doRefresh = function () {
+		$scope.feed.loadFeed().then(function () {
+			$scope.$broadcast('scroll.refreshComplete');
+		});
+	};
+});
+
+app.controller('PostCtrl', function (FeedService, $scope, $stateParams) {
+	console.log("Loading PostCtrl");
+    
+//    $scope.postId = $stateParams.id;
+//    $scope.post = FeedService.getEntry($scope.postId);
+
+	$scope.share = function () {
+		console.debug("Sharing post");
+	};
+
+	$scope.readMore = function () {
+		console.debug("Read more post");
+	};
+
+});
